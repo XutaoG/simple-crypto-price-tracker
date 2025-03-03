ad7c18c176344f922e2f67c22e1141887714e8ee
Xutao Gao <taogeeee@gmail.com>
2025-03-03 18:57:15 -0500

    Update next.js config images remote pattern

--- web-app/next.config.ts
+++ web-app/next.config.ts
@@ -3,7 +3,13 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
 	/* config options here */
 	images: {
-		domains: ["coin-images.coingecko.com"],
+		remotePatterns: [
+			{
+				protocol: "https",
+				hostname: "coin-images.coingecko.com",
+				pathname: "**",
+			},
+		],
 	},
 };
 
